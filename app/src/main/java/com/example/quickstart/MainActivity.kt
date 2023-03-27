b4c7fefc0c0c260d4ec90cc79e92a80a985a55b5
jeremyherrera <rocafuerteherrera@gmail.com>
2023-03-27 17:34:34 -0500

    se hacer un navigationdrawer en kotlin

--- app/src/main/java/com/example/quickstart/MainActivity.kt
+++ app/src/main/java/com/example/quickstart/MainActivity.kt
@@ -2,55 +2,64 @@ package com.example.quickstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.example.quickstart.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.Tab
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
-        private lateinit var biding : ActivityMainBinding
+    lateinit var toogle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        biding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(biding.root)
-        replaceFragment(Home())
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
 
 
-        biding.bottomNavigationView.setOnItemSelectedListener {
 
-        when(it.itemId){
+        toogle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
 
-                R.id.home -> replaceFragment(Home())
-                R.id.profile -> replaceFragment(Profile())
-                R.id.settings -> replaceFragment(settings())
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-          else ->{
+        navView.setNavigationItemSelectedListener {
 
-          }
+
+           when(it.itemId){
+
+              R.id.nav_home -> Toast.makeText(applicationContext,"clicked Home",Toast.LENGTH_SHORT).show()
+              R.id.nav_message -> Toast.makeText(applicationContext,"clicked Message",Toast.LENGTH_SHORT).show()
+              R.id.nav_sync -> Toast.makeText(applicationContext,"clicked Sync",Toast.LENGTH_SHORT).show()
+              R.id.nav_trash -> Toast.makeText(applicationContext,"clicked Trash",Toast.LENGTH_SHORT).show()
+              R.id.nav_settings -> Toast.makeText(applicationContext,"clicked Settings",Toast.LENGTH_SHORT).show()
+              R.id.nav_login -> Toast.makeText(applicationContext,"clicked Login",Toast.LENGTH_SHORT).show()
+              R.id.nav_share -> Toast.makeText(applicationContext,"clicked Share",Toast.LENGTH_SHORT).show()
+              R.id.nav_rate_us -> Toast.makeText(applicationContext,"clicked Rate us",Toast.LENGTH_SHORT).show()
+
+
+           }
+
+true
+
 
         }
-        true
 
+     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toogle.onOptionsItemSelected(item))
+
+            return true
+
+        return super.onOptionsItemSelected(item)
     }
-
-
-
-
-  }
-
-       private fun replaceFragment(fragment: Fragment){
-
-           val fragmentManager = supportFragmentManager
-           val fragmentTransaction = fragmentManager.beginTransaction()
-           fragmentTransaction.replace(R.id.frame_layout,fragment)
-           fragmentTransaction.commit()
-
-
-       }
 
 
 }
